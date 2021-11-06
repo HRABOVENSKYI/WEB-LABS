93b05ec034701e8f4fe22da23e3a06949ebf9dcf
HRABOVENSKYI <teodor.gr.2002@gmail.com>
2021-11-06 13:55:39 +0200

    Minor change

--- src/components/Cards/Wrapper.js
+++ src/components/Cards/Wrapper.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Wrapper = ({children}) => {
   return (
-    <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 pt-10">
+    <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-10">
       {children}
     </section>
   );
